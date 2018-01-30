9b6ed9fa46929687b6ea1a6a37872d609674753d
Tristan Bennett <tmbennett2011@msn.com>
2018-01-30 09:02:32 -0700

    css snap challenge demonstrating aspects of bootstrap

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<meta charset="UTF-8"/>
+			<link rel = "stylesheet" href="syle.css"/>
+			<title>CSS Snap Challenge</title>
+	</head>
+	<body>
+		<h1>CSS Snap Challenge</h1>
+		<div class = "row">
+			<div class = "col">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas sed tempus. Sed lectus vestibulum mattis ullamcorper. Mattis rhoncus urna neque viverra justo. Amet venenatis urna cursus eget nunc scelerisque. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Sagittis orci a scelerisque purus. Convallis posuere morbi leo urna molestie at. Sit amet justo donec enim diam vulputate ut. Volutpat diam ut venenatis tellus in metus vulputate eu. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque nec nam aliquam sem et tortor consequat id. Sed sed risus pretium quam vulputate dignissim suspendisse in est.</p>
+
+				<p>Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Ut ornare lectus sit amet est placerat in egestas. Quam elementum pulvinar etiam non quam lacus. Nulla facilisi morbi tempus iaculis. Nibh venenatis cras sed felis eget velit. Sagittis purus sit amet volutpat consequat mauris nunc. Dui accumsan sit amet nulla facilisi morbi tempus. Eget felis eget nunc lobortis mattis aliquam faucibus purus in. Lectus magna fringilla urna porttitor rhoncus dolor purus. Habitasse platea dictumst quisque sagittis. Adipiscing enim eu turpis egestas pretium. Odio tempor orci dapibus ultrices in iaculis nunc. Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Habitant morbi tristique senectus et netus et malesuada fames. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Est placerat in egestas erat.</p>
+
+				<p>Lacus sed viverra tellus in hac habitasse platea. Sagittis vitae et leo duis ut diam quam. Nulla facilisi cras fermentum odio eu feugiat. Arcu felis bibendum ut tristique et egestas quis. Proin fermentum leo vel orci. Malesuada fames ac turpis egestas. Integer feugiat scelerisque varius morbi enim nunc faucibus a. Eu scelerisque felis imperdiet proin fermentum leo. Massa eget egestas purus viverra accumsan in. Tincidunt dui ut ornare lectus sit. Tortor dignissim convallis aenean et tortor. Turpis massa sed elementum tempus egestas sed sed. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Nisl nisi scelerisque eu ultrices vitae. Sed blandit libero volutpat sed cras. Et ligula ullamcorper malesuada proin libero nunc consequat. Magna eget est lorem ipsum.</p>
+			</div>
+
+			<div class = "col">
+
+				<img src="impsum-image.jpg" alt="ipsum image"/>
+
+				<p>Blandit massa enim nec dui nunc mattis. Placerat duis ultricies lacus sed. Id aliquet lectus proin nibh nisl condimentum id. Mattis molestie a iaculis at erat pellentesque adipiscing. Nulla malesuada pellentesque elit eget gravida cum. Ullamcorper morbi tincidunt ornare massa eget egestas. Enim facilisis gravida neque convallis a cras semper auctor. Augue neque gravida in fermentum et sollicitudin. Proin nibh nisl condimentum id venenatis a condimentum vitae sapien. Aliquam purus sit amet luctus. Pellentesque sit amet porttitor eget dolor morbi non arcu risus. Iaculis eu non diam phasellus. Eget dolor morbi non arcu risus quis varius quam quisque. Faucibus ornare suspendisse sed nisi. Metus vulputate eu scelerisque felis imperdiet proin. At urna condimentum mattis pellentesque id nibh tortor. Magna etiam tempor orci eu lobortis elementum. Egestas dui id ornare arcu odio ut sem.</p>
+
+				<p>Eget magna fermentum iaculis eu non diam phasellus vestibulum. Auctor elit sed vulputate mi sit amet mauris commodo. Facilisi etiam dignissim diam quis enim. Habitasse platea dictumst quisque sagittis purus sit. Venenatis urna cursus eget nunc scelerisque viverra mauris in. Id faucibus nisl tincidunt eget nullam non nisi est sit. Mauris augue neque gravida in fermentum et sollicitudin. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Quis imperdiet massa tincidunt nunc pulvinar sapien et. Cras tincidunt lobortis feugiat vivamus. Suscipit adipiscing bibendum est ultricies integer quis auctor elit. Morbi tincidunt augue interdum velit euismod in. Felis eget velit aliquet sagittis id consectetur purus. Sed egestas egestas fringilla phasellus faucibus scelerisque. Et ligula ullamcorper malesuada proin.</p>
+
+				<p>Eu feugiat pretium nibh ipsum. Justo eget magna fermentum iaculis eu non. Congue nisi vitae suscipit tellus mauris a diam maecenas. Accumsan tortor posuere ac ut consequat semper. Consectetur lorem donec massa sapien faucibus et. Mattis pellentesque id nibh tortor. Orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Proin nibh nisl condimentum id. Diam sit amet nisl suscipit adipiscing bibendum. Lectus proin nibh nisl condimentum id venenatis. Sit amet commodo nulla facilisi nullam vehicula. Ante metus dictum at tempor commodo ullamcorper a lacus. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Porttitor lacus luctus accumsan tortor. Ut tellus elementum sagittis vitae et leo duis.</p>
+			</div>
+		</div>
+
+	</body>
+</html>
